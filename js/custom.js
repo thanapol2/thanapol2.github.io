3fbd50d4751a7e1fd06e9e4e8af3fc74992ba6f7
karnkung2 <thanapol.phu@gmail.com>
2018-10-09 08:17:34 +0700

    update 0.1
    update 0.1

--- js/custom.js
+++ js/custom.js
@@ -1,53 +1,53 @@
 (function($) {
-    "use strict";
+	"use strict";
 	
 	
 	/* ..............................................
 	Loader 
-    ................................................. */
+	................................................. */
 	
 	$(window).on('load', function() { 
 		$('.preloader').fadeOut(); 
 		$('#preloader').delay(550).fadeOut('slow'); 
 		$('body').delay(450).css({'overflow':'visible'});
 	});
-    	
+
 	/* ..............................................
     Navbar Bar
     ................................................. */
-	
-	$('.navbar-nav .nav-link').on('click', function() {
-		var toggle = $('.navbar-toggler').is(':visible');
-		if (toggle) {
-			$('.navbar-collapse').collapse('hide');
-		}
-	});
-	
+
+    $('.navbar-nav .nav-link').on('click', function() {
+    	var toggle = $('.navbar-toggler').is(':visible');
+    	if (toggle) {
+    		$('.navbar-collapse').collapse('hide');
+    	}
+    });
+
 	/* ..............................................
     Fixed Menu
     ................................................. */
     
-	$(window).on('scroll', function () {
-		if ($(window).scrollTop() > 50) {
-			$('.top-header').addClass('fixed-menu');
-		} else {
-			$('.top-header').removeClass('fixed-menu');
-		}
-	});
-	
+    $(window).on('scroll', function () {
+    	if ($(window).scrollTop() > 50) {
+    		$('.top-header').addClass('fixed-menu');
+    	} else {
+    		$('.top-header').removeClass('fixed-menu');
+    	}
+    });
+
 	/* ..............................................
     Gallery
     ................................................. */
-	
-	$(document).ready(function() {
-		$('.popup-gallery').magnificPopup({
-			delegate: 'a',
-			type: 'image',
-			tLoading: 'Loading image #%curr%...',
-			mainClass: 'mfp-img-mobile',
-			gallery: {
-				enabled: true,
-				navigateByImgClick: true,
+
+    $(document).ready(function() {
+    	$('.popup-gallery').magnificPopup({
+    		delegate: 'a',
+    		type: 'image',
+    		tLoading: 'Loading image #%curr%...',
+    		mainClass: 'mfp-img-mobile',
+    		gallery: {
+    			enabled: true,
+    			navigateByImgClick: true,
 				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
 			},
 			image: {
@@ -57,23 +57,36 @@
 				}
 			}
 		});
-	});
-	
+    });
+
 	/* ..............................................
     Smooth Scroll
     ................................................. */
-	
-	$('a[href*="#"]:not([href="#"])').on('click', function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
-		  var target = $(this.hash);
-			  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			  if (target.length) {
-				$('html,body').animate({
-				  scrollTop: target.offset().top - 65,
-				  }, 1000);
-				  return false;
-			  }
-		}
-	});
-	
+
+    $('a[href*="#"]:not([href="#"])').on('click', function() {
+    	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+    		var target = $(this.hash);
+    		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    		if (target.length) {
+    			$('html,body').animate({
+    				scrollTop: target.offset().top - 65,
+    			}, 1000);
+    			return false;
+    		}
+    	}
+    });
+
 }(jQuery));
+
+function initMap() {
+  // The location of Uluru
+  var primehotel = {lat: 13.73773, lng: 100.515307};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: primehotel});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: primehotel, map: map});
+}
+
+
+
